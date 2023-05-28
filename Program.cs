0a77c03052cb5411d9068345f191ae418a455d5c
Mehmed Lyotev <93915859+MehmedL@users.noreply.github.com>
2023-05-28 16:00:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿string stName = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+double grade = double.Parse(Console.ReadLine());
+Console.WriteLine($"Name: {stName}, Age: {age}, Grade: {grade:f2}");

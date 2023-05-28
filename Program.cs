30b8b0e474daf135fe19e7f6261cf00dfc7ccb4e
Mehmed Lyotev <93915859+MehmedL@users.noreply.github.com>
2023-05-28 16:00:56 +0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,4 +0,0 @@
-﻿string stName = Console.ReadLine();
-int age = int.Parse(Console.ReadLine());
-double grade = double.Parse(Console.ReadLine());
-Console.WriteLine($"Name: {stName}, Age: {age}, Grade: {grade:f2}");
